ab46870b4452f2aa1fa9e246911ef2890a240dbe
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-09-22 23:34:12 +0900

    에어컨 모아 프로젝트 시작

---
+++ src/main/java/com/airconmoa/airconmoa/AirconmoaApplication.java
@@ -0,0 +1,13 @@
+package com.airconmoa.airconmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirconmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirconmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airconmoa/airconmoa/AirconmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airconmoa.airconmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirconmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
